cbab962127bbd43da522edee458bf33b97b3b19f
BrianP8701 <brianp8701@gmail.com>
2024-04-04 21:48:52 -0700

    5th attempt

--- function_app.py
+++ function_app.py
@@ -1,8 +1,8 @@
 # function_app.py
 import azure.functions as func 
-from api.authentication.login.main import bp as login_bp
-from api.authentication.signup.main import bp as signup_bp
-from api.admin.delete_user.main import bp as delete_user_bp
+from login import bp as login_bp
+from signup import bp as signup_bp
+from delete_user import bp as delete_user_bp
 
 
 app = func.FunctionApp() 
